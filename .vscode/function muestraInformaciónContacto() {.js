f597f92b01891d09fdb8565e1b946c9211492b89
Vaalesitaa <jacyaravalentina.icccp@gmail.com>
2022-08-18 09:00:56 -0400

    segundo trabajo coding dojo

---
+++ .vscode/function muestraInformaciónContacto() {.js
@@ -0,0 +1,25 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+// El console.log imprimira la imformacion de los contactos 
+
+
+
+
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+// El console.log imprimira que se conjelen las verduras y el helado 
+
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+// El console.log imprimira mover a la libreria 
